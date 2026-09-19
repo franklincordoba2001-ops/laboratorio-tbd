5ae6e79f52ff100e76206480493a23d3d2150822
franklin cordoba <franklincordoba2001@gmail.com>
2026-09-19 17:01:55 -0500

    feat: agregar método resta() a Calculator con su test

--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
 from main import Calculator
 
 
-def test_suma():
-    calc = Calculator()
-    assert calc.suma(2, 3) == 5
+def test_sums_2_numbers():
+    assert Calculator().suma(2, 2) == 4
+
+
+def test_resta_2_numbers():
+    assert Calculator().resta(5, 3) == 2
